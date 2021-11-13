01f2ddc9dfe36924968b49e2fa16db2b785b8406
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-11-12 17:27:15 -0800

    added dateAdded to round

--- server/models/Round.js
+++ server/models/Round.js
@@ -2,6 +2,10 @@ const {Schema, model } = require('mongoose');
 
 const Round = new Schema({
     scores: [Number],
+    dateAdded: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 module.exports = Round;
